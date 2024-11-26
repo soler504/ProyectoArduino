21c0e9322569f0e4892f638fc83459b620ace2cb
DevHN504 <cindycarolinasoler@outlook.es>
2024-11-26 16:47:40 -0600

    sangria

--- carro/carro.ino
+++ carro/carro.ino
@@ -1,20 +1,25 @@
 #include <SoftwareSerial.h>
 #include "Lista.h"
+
 SoftwareSerial mySerial(11,10);  
- char inputByte = "";
- char ul = "";
- bool grabar_u=false;
- bool ejecutar_w=false;
+char inputByte = "";
+char ul = "";
+bool grabar_u=false;
+bool ejecutar_w=false;
+
 int Pin_2 = 4;          
 int Pin_3 = 2;        
 int Pin_4 = 6;
-int Pin_5 = 7; 
+int Pin_5 = 7;
+
 Lista  move;
 unsigned long tiempo_actual;
 unsigned long tiempo_anterior = 0;
 unsigned long delta_tiempo;
+
 int contador = 0;
 int opcion = 0;
+
 void setup() {
   mySerial.begin(9600);
   Serial.begin(9600);
@@ -29,57 +34,65 @@ void loop() {
  // Esperamos 1 segundo
  // delay(1000);
   cronometro();
-   if(mySerial.available()){
-    inputByte = mySerial.read();
-    if(inputByte == 'M'){
-      grabar_u == true;
-    }else if(inputByte == 'm'){
-      grabar_u == false;
-    }
-    if(inputByte == 'N'){
-      ejecutar_w == true;
-    }else if('n'){
-      ejecutar_w == false;
-    }
-    if(grabar_u == true){
-        Serial.println(inputByte);
-        movimiento(inputByte);
-        if(inputByte == 'S'){
-          move.agregar(ul, contador);
-        }
-        contador = 0;
-    }
-    if(ejecutar_w == true){
-        move.presentarF();
-    }
-    Serial.println(inputByte);
-        movimiento(inputByte);
-        
-        //move.agregar(ul, contador);
-        contador = 0;
-   ul = inputByte;    
+
+    if(mySerial.available()){
+      inputByte = mySerial.read();
+      if(inputByte == 'M'){
+        grabar_u == true;
+      }else if(inputByte == 'm'){
+        grabar_u == false;
+      }
+
+      if(inputByte == 'N'){
+        ejecutar_w == true;
+      }else if('n'){
+        ejecutar_w == false;
+      }
+
+      if(grabar_u == true){
+          Serial.println(inputByte);
+          movimiento(inputByte);
+          if(inputByte == 'S'){
+            move.agregar(ul, contador);
+          }
+          contador = 0;
+      }
+
+      if(ejecutar_w == true){
+          move.presentarF();
+      }
+
+      Serial.println(inputByte);
+      movimiento(inputByte);
+          
+      //move.agregar(ul, contador);
+      contador = 0;
+      ul = inputByte;    
    }
    
 }
 void movimiento(char m){
     if(m=='S'){         
-       stop1();
+       stop();
     }
 
-      if(m=='B'){
-        back();
-      }
-      if(m=='F'){
-        front();
-      }
-      if(m=='L'){
-       left();
-      }
-      if(m=='R'){
-        right();
-      }
+    if(m=='B'){
+      back();
+    }
+    
+    if(m=='F'){
+      front();
+    }
+
+    if(m=='L'){
+     left();
+    }
+
+    if(m=='R'){
+      right();
+    }
 }
-void stop1(){
+void stop(){
         digitalWrite(Pin_2,LOW);
         digitalWrite(Pin_4,LOW);
         digitalWrite(Pin_3,LOW);
@@ -90,18 +103,22 @@ void back(){
          digitalWrite(Pin_4,HIGH);
          digitalWrite(Pin_3,HIGH);
       }
+
 void front(){
         digitalWrite(Pin_2,HIGH);
         digitalWrite(Pin_5,HIGH);
       }
+
 void left(){
         digitalWrite(Pin_5,HIGH); 
         digitalWrite(Pin_3,HIGH);
       }
+
 void right(){
         digitalWrite(Pin_4,HIGH);
         digitalWrite(Pin_2,HIGH);
       }
+
 void cronometro(){
   tiempo_actual = millis();
     delta_tiempo = tiempo_actual - tiempo_anterior;
